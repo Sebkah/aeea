c59e6ec8e93abfe9964cf86cea981741bc9cbd30
sebkah <sebastien.kt@gmail.com>
2023-07-20 16:36:11 +0200

    Initial

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,6 @@
 import Head from 'next/head';
-import Image from 'next/image';
-import { Inter } from 'next/font/google';
-import styles from '@/styles/Home.module.css';
 
-const inter = Inter({ subsets: ['latin'] });
+import { Inter } from 'next/font/google';
 
 export default function Home() {
   return (
@@ -14,7 +11,7 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main>Hello Théo Chassouant à l'appareil</main>
+      <main>Hello Théo Chassouant au téléphone</main>
     </>
   );
 }
